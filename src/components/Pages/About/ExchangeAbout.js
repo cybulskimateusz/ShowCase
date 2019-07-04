a3eda72760583ee07bed8d7975bed7739bb4709f
unknown <cybulskimateusz@icloud.com>
2019-07-04 18:52:13 +0200

    about+graphCMS

---
+++ src/components/Pages/About/ExchangeAbout.js
@@ -0,0 +1,51 @@
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import React from "react";
+import lateRealisticTyper from '../../lateRealisticTyper';
+
+const ExchangeAbout = () => (
+  <Query
+    query={gql`
+{
+	abouts(where:{id:"cjxopkjixoi260d538urs4tlz"}){
+    id
+    header
+    content
+    name
+    surname
+    age
+    city
+    profilephoto{
+      handle
+    }
+  }
+}
+    `}
+  >
+    {({ loading, error, data }) => {
+        
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error </p>;
+      
+      return data.abouts.map(({ id,name, surname, age, city, profilephoto,header, content}) => (
+          <article id="about" key={id}>
+            <img src={`https://media.graphcms.com/${profilephoto.handle}`} className="profile_picture" alt="Hello, It's me" />
+            <section id="hello_world">
+                <h1 id="about_header"></h1>
+        <br></br> <p id="about_content"></p>
+            </section>
+            <div id="personal_data"><p><b>Name: </b>{name}</p><p><b>Surname: </b>{surname}</p><p><b>Age: </b>{age}</p><p><b>City: </b>{city}</p></div>
+              {
+                  data.abouts.map(function({header, content}){
+                      lateRealisticTyper(header.toString(),"#about_header",0)
+                      lateRealisticTyper(content.toString(),"#about_content",2500)
+                  })
+              }
+        </article>
+        ));
+            
+    }}
+  </Query>
+);
+
+export default ExchangeAbout;
--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -4,24 +4,23 @@ import './about.css';
 import NavBar_color from '../../NavBar/NavBar_color';
 import './about.css'
 import ReactScrollWheelHandler from "react-scroll-wheel-handler";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import ExchangeAbout from "./ExchangeAbout";
+
+
+const client = new ApolloClient({
+  uri: "https://api-euwest.graphcms.com/v1/cjxlm9rgg00bi01hwbfp58wps/master"
+});
 
 class About extends Component{
     
     render(){
         NavBar_color("#4d4d4d");
-        
-    lateRealisticTyper("First, you tell me how you got it. How, where, and when. Alright, take a seat. (Marty stays standing. Biff walks over to the painting of himself. He turns to Marty.) Sit down! (Marty does) November 12th 1955, that was when. (During the following Biff reveals the painting is really a safe. He opens the safe.) November 12th 1955? That was the day I went back... (covering) that was the day of the famous Hill Valley lightning storm. You know your history, very good. I'll never forget that Saturday. I'd just picked my car up from the shop because I'd enrolled it in a drag race a few days earlier.","#about_content",1500);
-    lateRealisticTyper("Hello World, I'm Matthew","#about_header",0);
     return (
-        
-        <article id="about">
-            <img className="profile_picture" alt="Hello, It's me" src="src/profile.jpg"/>
-            <section id="hello_world">
-                <h1 id="about_header"> </h1>
-        <br></br> <p id="about_content"></p>
-            </section>
-            <div id="personal_data"><p><b>Name: </b>Mateusz</p><p><b>Surname: </b>Cybulski</p><p><b>Age: </b>19</p><p><b>City: </b>Łódź</p></div>
-        </article>
+        <ApolloProvider client={client}>
+          <ExchangeAbout />
+        </ApolloProvider>
     );
 }
 }
