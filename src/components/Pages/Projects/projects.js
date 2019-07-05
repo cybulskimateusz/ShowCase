ae409e600bf9604a10fc699b37267518d3d52270
unknown <cybulskimateusz@icloud.com>
2019-07-05 15:02:36 +0200

    TimelineMax in projects

--- src/components/Pages/Projects/projects.js
+++ src/components/Pages/Projects/projects.js
@@ -5,60 +5,83 @@ import ExchangePosts from "./ExchangePosts";
 import Header from "../../Header/Header";
 import "./projects.css";
 import NavBar_color from "../../NavBar/NavBar_color";
-import ReactScrollWheelHandler from "react-scroll-wheel-handler"; 
-import TweenMax from 'gsap'; 
-import scrollTo from '../../../../node_modules/gsap/ScrollToPlugin'; 
-import $ from 'jquery'
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
+import TweenMax from "gsap";
+import scrollTo from "../../../../node_modules/gsap/ScrollToPlugin";
+import { TimelineMax } from "gsap";
+import $ from "jquery";
+
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjxlm9rgg00bi01hwbfp58wps/master"
 });
+const tl = new TimelineMax();
 
 class Projects extends Component {
   render() {
     NavBar_color("#4d4d4d");
-    document.querySelector("body").style.color = "#28ff00"
-    
+    document.querySelector("body").style.color = "#28ff00";
+
     var currentLocation = 1;
     var nextLocation = 2;
+
+    setTimeout(function() {
+      tl.set(".project_container", { opacity: 0, y: 300 }).to("#post_1", 0.2, {
+        opacity: 1,
+        y: 0
+      });
+    }, 1000);
     return (
       <>
         <Header preText="import.all.my." text="projects" postText="_" />
         <ReactScrollWheelHandler
-            upHandler={() => {
-                if(currentLocation<=nextLocation) nextLocation = currentLocation - 1
-                
-                var where = "#post_"+nextLocation
-                
-               if(document.getElementById('projects').contains(document.querySelector(where))){ 
-                    TweenMax.to("#projects",.4,{scrollTo:{y:where, offsetY:0}})
-                    currentLocation--
-                    console.log(where)
-               }else{
-                    window.location = "#/"
-                }
+          upHandler={() => {
+            if (currentLocation <= nextLocation)
+              nextLocation = currentLocation - 1;
+
+            var whereGo = "#post_" + nextLocation;
+            var where = "#post_" + currentLocation;
+            if (
+              document
+                .getElementById("projects")
+                .contains(document.querySelector(whereGo))
+            ) {
+              tl.to(where, 0.2, { y: 300, opacity: 0 }).to(whereGo, 0.2, {
+                y: 0,
+                opacity: 1
+              });
+              currentLocation--;
+            } else {
+              window.location = "#/";
             }
-                             }
-            downHandler={() => {
-                if(currentLocation>=nextLocation) nextLocation = currentLocation + 1
-                
-                var where = "#post_"+nextLocation
-                         
-                if(document.getElementById('projects').contains(document.querySelector(where))){
-                    TweenMax.to("#projects",.4,{scrollTo:{y:where, offsetY:0}})
-                    currentLocation++
-                    console.log(where)
-                }else{
-                    window.location = "#/abilities"
-                }
-                               }}
-            >
-        <div id="projects" className="projects">
-        <ApolloProvider client={client}>
-          <ExchangePosts />
-        </ApolloProvider>
-            </div>
+          }}
+          downHandler={() => {
+            if (currentLocation >= nextLocation)
+              nextLocation = currentLocation + 1;
+
+            var where = "#post_" + currentLocation;
+            var whereGo = "#post_" + nextLocation;
+
+            if (
+              document
+                .getElementById("projects")
+                .contains(document.querySelector(whereGo))
+            ) {
+              tl.to(where, 0.2, { y: -300, opacity: 0 }).to(whereGo, 0.2, {
+                y: 0,
+                opacity: 1
+              });
+              currentLocation++;
+            } else {
+              window.location = "#/abilities";
+            }
+          }}
+        >
+          <div id="projects" className="projects">
+            <ApolloProvider client={client}>
+              <ExchangePosts />
+            </ApolloProvider>
+          </div>
         </ReactScrollWheelHandler>
-            
       </>
     );
   }
