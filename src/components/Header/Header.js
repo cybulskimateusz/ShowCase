ee69b1ccc47ab48580227cb5c8978c2d4d8e996b
unknown <cybulskimateusz@icloud.com>
2019-08-10 18:51:11 +0200

    header classname fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ function Header({preText, text, postText}){
     lateRealisticTyper(posttext,"#postText",4000);
     
     return (
-        <header id="header" class="h-100 my-0 ml-0 mr-auto position-absolute d-lg-flex d-none">
+        <header id="header" className="h-100 my-0 ml-0 mr-auto position-absolute d-lg-flex d-none">
             <div className="d-flex position-relatve w-100 h-100">
                 <p className="text_header mt-auto mr-auto mb-4 ml-1 w-100"><span className="h2"id="preText"></span><b><span className="h2 font-weight-bolder" id="text"></span></b><span className="h2" id="postText"></span></p>
             </div>
